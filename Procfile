e1da461ac7d3b58409db0e0121a328761c05594c
Aditya Bhonsle <51963138+bhonsleaditya1@users.noreply.github.com>
2022-01-30 14:40:38 +0530

    Config Update

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: java -jar build/server/webapp-runner-*.jar build/libs/*.war
+stage: ./gradlew stage
--- src/main/java/com/spring/jdbc/TestDBServlet.java
+++ src/main/java/com/spring/jdbc/TestDBServlet.java
@@ -2,13 +2,14 @@ package com.spring.jdbc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.net.URI;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.sql.*;
 
@@ -18,29 +19,31 @@ import java.sql.*;
 @WebServlet("/TestDBServlet")
 public class TestDBServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
-    @Value("${spring.datasource.url}")
-    private String jdbcUrl;
-
+    
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
         // setup connection variables
         //String user = "springstudent";
         //String pass = "springstudent";
-
-        
+       
         String driver = "org.postgresql.Driver";
 
         // get connection
         try{
             PrintWriter out = response.getWriter();
-            out.println("Connecting to database!!"+jdbcUrl);
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            out.println("Connecting to database!!");
             Class.forName(driver);
-            Connection connection = DriverManager.getConnection(jdbcUrl);
+            Connection connection = DriverManager.getConnection(dbUrl,username,password);
             out.println("Success!!");
             connection.close();
         }catch (Exception e){
